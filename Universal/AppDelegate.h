ef79de5da2d052429249a4dda2ffc78620519062
杜晓星 <1255322700@qq.com>
2015-08-31 15:48:34 +0800

    建立工程

---
+++ Universal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Universal
+//
+//  Created by emiaobao on 15/8/31.
+//  Copyright (c) 2015年 emiaobao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
