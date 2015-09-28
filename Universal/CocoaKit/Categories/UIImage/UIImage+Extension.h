ba7afe55558b946acd355958ffe685c04aac1124
杜晓星 <1255322700@qq.com>
2015-09-28 19:59:42 +0800

    增加部分注释

--- Universal/CocoaKit/Categories/UIImage/UIImage+Extension.h
+++ Universal/CocoaKit/Categories/UIImage/UIImage+Extension.h
@@ -10,10 +10,33 @@
 
 @interface UIImage (Extension)
 
+/**
+ *  本地读取图片
+ *
+ *  @param imageFullName <#imageFullName description#>
+ *
+ *  @return <#return value description#>
+ */
 + (UIImage*)noCacheWithName:(NSString*)imageFullName;
 
+/**
+ *  毛玻璃效果
+ *
+ *  @param blurPercent <#blurPercent description#>
+ *
+ *  @return <#return value description#>
+ */
 - (UIImage*)blurPercent:(CGFloat)blurPercent;
 
+/**
+ *  生成二维码
+ *
+ *  @param string <#string description#>
+ *  @param w      <#w description#>
+ *  @param h      <#h description#>
+ *
+ *  @return <#return value description#>
+ */
 + (UIImage*)QRCodeImageWithString:(NSString*)string imageWeight:(CGFloat)w imageHeight:(CGFloat)h;
 /**
  *  存入本地相册
