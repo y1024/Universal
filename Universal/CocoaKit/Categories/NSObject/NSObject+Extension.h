dd5714c71dc3dabbb5e5b92cb6afb31001d54550
杜晓星 <1255322700@qq.com>
2016-01-15 02:27:12 +0800

    runtime 头文件 注释完毕

--- Universal/CocoaKit/Categories/NSObject/NSObject+Extension.h
+++ Universal/CocoaKit/Categories/NSObject/NSObject+Extension.h
@@ -127,6 +127,16 @@ BOOL __clsConformsToProtocol(Class cls, Protocol *protocol);
  *  @return
  */
 BOOL __clsRespondsToSelector(Class cls, SEL sel);
+
+/**
+ *  获取类的 协议列表
+ *
+ *  @param cls
+ *  @param outCount
+ *
+ *  @return
+ */
+Protocol * __unsafe_unretained *__clscopyProtocolList(Class cls, unsigned int *outCount);
 /**
  *  类 是否是元
  *
@@ -135,7 +145,121 @@ BOOL __clsRespondsToSelector(Class cls, SEL sel);
  *  @return 
  */
 BOOL __clsIsMetaClass(Class cls);
+/**
+ *  获取类的属性
+ *
+ *  @param cls
+ *  @param name
+ *
+ *  @return 
+ */
+objc_property_t __clsGetProperty(Class cls, const char *name);
+/**
+ *  获取类的属性列表
+ *
+ *  @param cls
+ *  @param outCount
+ *
+ *  @return
+ */
+objc_property_t *__clsCopyPropertyList(Class cls, unsigned int *outCount);
+/**
+ *  获取 类 变量布局
+ *
+ *  @param cls
+ *
+ *  @return
+ */
+const uint8_t *__clsIvarLayout(Class cls);
+/**
+ *  获取类 弱引用变量 布局
+ *
+ *  @param cls
+ *
+ *  @return
+ */
+const uint8_t *__clsWeakIvarLayout(Class cls);
+/**
+ *  类增加方法
+ *
+ *  @param cls
+ *  @param name
+ *  @param imp
+ *  @param types
+ *
+ *  @return 
+ */
+BOOL __clsAddMethod(Class cls, SEL name, IMP imp,
+                    const char *types);
+/**
+ *  替换类 某个方法的实现
+ *
+ *  @param cls
+ *  @param name
+ *  @param imp
+ *  @param types
+ *
+ *  @return
+ */
+IMP __clsReplaceMethod(Class cls, SEL name, IMP imp,
+                       const char *types);
+/**
+ *  给 类 增加 变量
+ *
+ *  @param cls
+ *  @param name
+ *  @param size
+ *  @param alignment
+ *  @param types
+ *
+ *  @return 
+ */
+BOOL __clsAddIvar(Class cls, const char *name, size_t size,
+                  uint8_t alignment, const char *types);
+/**
+ *  给 类 增加协议
+ *
+ *  @param cls
+ *  @param protocol
+ *
+ *  @return     
+ */
+BOOL __clsAddProtocol(Class cls, Protocol *protocol);
 
+/**
+ *  给 类 添加 属性
+ *
+ *  @param cls
+ *  @param name
+ *  @param attributes
+ *  @param attributeCount
+ *
+ *  @return 
+ */
+BOOL __clsAddProperty(Class cls, const char *name, const objc_property_attribute_t *attributes, unsigned int attributeCount);
+/**
+ *  替换类的属性
+ *
+ *  @param cls
+ *  @param name
+ *  @param attributes
+ *  @param attributeCount 
+ */
+void __clsReplaceProperty(Class cls, const char *name, const objc_property_attribute_t *attributes, unsigned int attributeCount);
+/**
+ *  设置 变量的 layout
+ *
+ *  @param cls
+ *  @param layout   
+ */
+void __clsSetIvarLayout(Class cls, const uint8_t *layout);
+/**
+ *  设置 弱引用 变量的 layout
+ *
+ *  @param cls
+ *  @param layout 
+ */
+void __clsSetWeakIvarLayout(Class cls, const uint8_t *layout);
 
 /**** 实例方法 *******/
 
@@ -201,6 +325,403 @@ void __objSetIvar(id obj, Ivar ivar, id value);
  */
 Class __objcGetClass(const char *name);
 Class __objcGetMetaClass(const char *name);
+
+Class __objcAllocateClassPair(Class superclass, const char *name,
+                              size_t extraBytes);
+/**
+ *  注册 一个类
+ *
+ *  @param cls
+ */
+void __objcRegisterClassPair(Class cls);
+/**
+ *  销毁 某个类 如果这个类的子类存在 不能去销毁这个类
+ *
+ *  @param cls 
+ */
+void __objcDisposeClassPair(Class cls);
+
+/**
+ *  获取 方法名字
+ *
+ *  @param m
+ *
+ *  @return 
+ */
+SEL __methodName(Method m);
+/**
+ *  获取方法的实现
+ *
+ *  @param m
+ *
+ *  @return 
+ */
+IMP __methodImplementation(Method m);
+/**
+ *  获取 方法返回类型
+ *
+ *  @param m
+ *
+ *  @return
+ */
+const char * __methodGetTypeEncoding(Method m);
+/**
+ *  返回方法参数个数
+ *
+ *  @param m
+ *
+ *  @return
+ */
+unsigned int __methodNumberOfArguments(Method m);
+/**
+ *  复制 方法返回 参数  
+ *
+ *  @param m
+ *
+ *  @return
+ */
+char *__methodCopyReturnType(Method m);
+/**
+ *  
+ *
+ *  @param m
+ *  @param dst
+ *  @param dst_len
+ */
+void __methodReturnType(Method m, char *dst, size_t dst_len);
+
+/**
+ *  设置 方法的实现
+ *
+ *  @param m
+ *  @param imp
+ *
+ *  @return 
+ */
+IMP __methodSetImplementation(Method m, IMP imp);
+/**
+ *  交换方法
+ *
+ *  @param m1
+ *  @param m2   
+ */
+void __methodExchangeImplementations(Method m1, Method m2);
+
+/* Working with Methods */
+
+
+
+/* Working with Instance Variables */
+/**
+ *  获取 ivar的名字
+ *
+ *  @param v
+ *
+ *  @return 
+ */
+const char *__ivarName(Ivar v);
+/**
+ *  变量的 编码格式
+ *
+ *  @param v
+ *
+ *  @return
+ */
+const char *__ivarTypeEncoding(Ivar v);
+/**
+ *  获取 变量的偏移量
+ *
+ *  @param v
+ *
+ *  @return
+ */
+ptrdiff_t __ivarOffset(Ivar v);
+
+
+/* Working with Instance Variables */
+
+
+
+/* Working with Properties */
+/**
+ *  获取属性名字
+ *
+ *  @param 
+ *
+ *  @return
+ */
+const char *__propertyName(objc_property_t property);
+
+const char *__propertyAttributes(objc_property_t property);
+
+objc_property_attribute_t *__propertyCopyAttributeList(objc_property_t property, unsigned int *outCount);
+
+char *__propertyCopyAttributeValue(objc_property_t property, const char *attributeName);
+
+
+/* Working with Properties */
+
+
+
+
+/* Working with Protocols */
+
+/**
+ *  获取 协议
+ *
+ *  @param name
+ *
+ *  @return
+ */
+Protocol *__objcProtocol(const char *name);
+
+/**
+ *  获取 协议数组
+ *
+ *  @param outCount
+ *
+ *  @return
+ */
+Protocol * __unsafe_unretained *__objcCopyProtocolList(unsigned int *outCount);
+
+/**
+ *  是否 依赖 另外一个协议
+ *
+ *  @param proto
+ *  @param other
+ *
+ *  @return
+ */
+BOOL __protocolConformsToProtocol(Protocol *proto, Protocol *other);
+/**
+ *  判断 协议是否相同
+ *
+ *  @param proto
+ *  @param other
+ *
+ *  @return
+ */
+BOOL __protocolEqual(Protocol *proto, Protocol *other);
+/**
+ *  获取协议名字
+ *
+ *  @param p
+ *
+ *  @return
+ */
+const char *__protocolName(Protocol *p);
+/**
+ *  获取 协议方法描述
+ *
+ *  @param p
+ *  @param aSel
+ *  @param isRequiredMethod
+ *  @param isInstanceMethod
+ *
+ *  @return
+ */
+struct objc_method_description __protocolMethodDescription(Protocol *p, SEL aSel, BOOL isRequiredMethod, BOOL isInstanceMethod);
+/**
+ *  复制 协议方法描述 数组
+ *
+ *  @param p
+ *  @param isRequiredMethod
+ *  @param isInstanceMethod
+ *  @param outCount
+ *
+ *  @return
+ */
+struct objc_method_description *__protocolCopyMethodDescriptionList(Protocol *p, BOOL isRequiredMethod, BOOL isInstanceMethod, unsigned int *outCount);
+/**
+ *  获取 协议的 属性
+ *
+ *  @param proto
+ *  @param name
+ *  @param isRequiredProperty
+ *  @param isInstanceProperty
+ *
+ *  @return
+ */
+objc_property_t __protocolProperty(Protocol *proto, const char *name, BOOL isRequiredProperty, BOOL isInstanceProperty);
+/**
+ *  复制 协议的属性数组
+ *
+ *  @param proto
+ *  @param outCount
+ *
+ *  @return
+ */
+objc_property_t *__protocolCopyPropertyList(Protocol *proto, unsigned int *outCount);
+/**
+ * 返回 一个协议里面的 协议数组
+ *
+ *  @param proto
+ *  @param outCount
+ *
+ *  @return
+ */
+Protocol *__unsafe_unretained *__protocolCopyProtocolList(Protocol *proto, unsigned int *outCount);
+
+/**
+ *  初始化 一个 协议
+ *
+ *  @param name
+ *
+ *  @return
+ */
+Protocol *__objcAllocateProtocol(const char *name);
+/**
+ *  注册 一个 协议
+ *
+ *  @param proto
+ */
+void __objcRegisterProtocol(Protocol *proto);
+
+/**
+ *  给 协议增加 方法描述
+ *
+ *  @param proto
+ *  @param name
+ *  @param types
+ *  @param isRequiredMethod
+ *  @param isInstanceMethod
+ */
+void __protocolAddMethodDescription(Protocol *proto, SEL name, const char *types, BOOL isRequiredMethod, BOOL isInstanceMethod);
+/**
+ *  给协议 增加协议
+ *
+ *  @param proto
+ *  @param addition
+ */
+void __protocolAddProtocol(Protocol *proto, Protocol *addition);
+
+/**
+ *  协议增加 属性
+ *
+ *  @param proto
+ *  @param name
+ *  @param attributes
+ *  @param attributeCount
+ *  @param isRequiredProperty
+ *  @param isInstanceProperty
+ */
+void __protocolAddProperty(Protocol *proto, const char *name, const objc_property_attribute_t *attributes, unsigned int attributeCount, BOOL isRequiredProperty, BOOL isInstanceProperty);
+/* Working with Protocols */
+
+
+
+/* Working with Libraries */
+
+/**
+ *  获取 动态加载的 framework
+ *
+ *  @param outCount <#outCount description#>
+ *
+ *  @return <#return value description#>
+ */
+const char **__objcCopyImageNames(unsigned int *outCount);
+
+/**
+ *  获取 包含 cls的 framework
+ *
+ *  @param cls
+ *
+ *  @return
+ */
+const char *__clsImageName(Class cls);
+
+/**
+ *  获取 framework 的所有 cls
+ *
+ *  @param image    <#image description#>
+ *  @param outCount <#outCount description#>
+ *
+ *  @return <#return value description#>
+ */
+const char **__objcCopyClassNamesForImage(const char *image,
+                                          unsigned int *outCount);
+
+
+
+/* Working with Libraries */
+
+
+
+
+/* Working with Selectors */
+
+/**
+ *  获取 SEL 名字
+ *
+ *  @param sel
+ *
+ *  @return
+ */
+const char *__selName(SEL sel);
+
+/**
+ *  通过名字 找到 SEL
+ *
+ *  @param str
+ *
+ *  @return
+ */
+SEL __selUid(const char *str);
+
+/**
+ *  注册 SEL
+ *
+ *  @param str
+ *
+ *  @return
+ */
+SEL __selRegisterName(const char *str);
+
+/**
+ *  比较 两个SEL 是否相同
+ *
+ *  @param lhs
+ *  @param rhs
+ *
+ *  @return
+ */
+BOOL __selEqual(SEL lhs, SEL rhs);
+
+/* Working with Selectors */
+
+
+
+/* Associative References */
+
+/**
+ *  设置 关联 obj
+ *
+ *  @param object
+ *  @param key
+ *  @param value
+ *  @param policy
+ */
+void __objcSetAssociatedObject(id object, const void *key, id value, objc_AssociationPolicy policy);
+/**
+ *  取的关联 obj
+ *
+ *  @param object
+ *  @param key
+ *
+ *  @return
+ */
+id __objcAssociatedObject(id object, const void *key);
+/**
+ *  移除 关联 obj
+ *
+ *  @param object
+ */
+void __objcRemoveAssociatedObjects(id object);
+
+/* Associative References */
+
+
+
 
 + (BOOL)methodSwizzle:(SEL)originalSelector withMethod:(SEL)swizzledSelector error:(NSError **)error;
 
--- Universal/CocoaKit/Categories/NSObject/NSObject+Extension.m
+++ Universal/CocoaKit/Categories/NSObject/NSObject+Extension.m
@@ -74,6 +74,68 @@ Method *__clsCopyMethodList(Class cls, unsigned int *outCount)
 {
     return class_copyMethodList(cls,outCount);
 }
+Protocol * __unsafe_unretained *__clscopyProtocolList(Class cls, unsigned int *outCount)
+{
+    return class_copyProtocolList(cls, outCount);
+}
+
+objc_property_t __clsGetProperty(Class cls, const char *name)
+{
+    return class_getProperty(cls, name);
+}
+objc_property_t *__clsCopyPropertyList(Class cls, unsigned int *outCount)
+{
+    return class_copyPropertyList(cls, outCount);
+}
+
+const uint8_t *__clsIvarLayout(Class cls)
+{
+    return class_getIvarLayout(cls);
+}
+const uint8_t *__clsWeakIvarLayout(Class cls)
+{
+    return class_getWeakIvarLayout(cls);
+}
+
+BOOL __clsAddMethod(Class cls, SEL name, IMP imp,
+                     const char *types)
+{
+    return class_addMethod(cls,name,imp,types);
+
+}
+IMP __clsReplaceMethod(Class cls, SEL name, IMP imp,
+                        const char *types)
+{
+    return class_replaceMethod(cls,name, imp,types);
+
+}
+BOOL __clsAddIvar(Class cls, const char *name, size_t size,
+                   uint8_t alignment, const char *types)
+{
+    return  class_addIvar(cls,name, size,alignment,types);
+}
+
+BOOL __clsAddProtocol(Class cls, Protocol *protocol)
+{
+    return class_addProtocol(cls,protocol);
+}
+BOOL __clsAddProperty(Class cls, const char *name, const objc_property_attribute_t *attributes, unsigned int attributeCount)
+{
+    return class_addProperty(cls,name,attributes,attributeCount);
+}
+
+void __clsReplaceProperty(Class cls, const char *name, const objc_property_attribute_t *attributes, unsigned int attributeCount)
+{
+    return class_replaceProperty(cls,name,attributes,attributeCount);
+}
+void __clsSetIvarLayout(Class cls, const uint8_t *layout)
+{
+    return class_setIvarLayout(cls,layout);
+}
+void __clsSetWeakIvarLayout(Class cls, const uint8_t *layout)
+{
+    return  class_setWeakIvarLayout(cls,layout);
+}
 
 BOOL __clsConformsToProtocol(Class cls, Protocol *protocol)
 {
@@ -145,6 +207,300 @@ Class *__objcCopyClassList(unsigned int *outCount)
     return objc_copyClassList(outCount);
 }
 
+/* Adding Classes */
+
+Class __objcAllocateClassPair(Class superclass, const char *name,
+                             size_t extraBytes)
+{
+    return objc_allocateClassPair(superclass,name,extraBytes);
+}
+void __objcRegisterClassPair(Class cls)
+{
+    return objc_registerClassPair(cls);
+}
+Class __objcDuplicateClass(Class original, const char *name, size_t extraBytes)
+{
+    return objc_duplicateClass(original,name,extraBytes);
+}
+
+void __objcDisposeClassPair(Class cls)
+{
+    return objc_disposeClassPair(cls);
+}
+/* Adding Classes */
+
+
+
+/* Working with Methods */
+
+SEL __methodName(Method m)
+{
+    return method_getName(m);
+}
+
+IMP __methodImplementation(Method m)
+{
+    return method_getImplementation(m);
+}
+
+const char * __methodGetTypeEncoding(Method m)
+{
+    return method_getTypeEncoding(m);
+}
+
+unsigned int __methodNumberOfArguments(Method m)
+{
+    return method_getNumberOfArguments(m);
+}
+
+char *__methodCopyReturnType(Method m)
+{
+    return method_copyReturnType(m);
+}
+
+void __methodReturnType(Method m, char *dst, size_t dst_len)
+{
+    return method_getReturnType(m, dst, dst_len);
+}
+
+struct objc_method_description *__methodDescription(Method m)
+{
+    return method_getDescription(m);
+}
+
+IMP __methodSetImplementation(Method m, IMP imp)
+{
+    return method_setImplementation(m, imp);
+}
+void __methodExchangeImplementations(Method m1, Method m2)
+{
+    return method_exchangeImplementations(m1, m2);
+}
+/* Working with Methods */
+
+
+
+/* Working with Instance Variables */
+
+const char *__ivarName(Ivar v)
+{
+    return ivar_getName(v);
+}
+const char *__ivarTypeEncoding(Ivar v)
+{
+    return ivar_getTypeEncoding(v);
+}
+
+ptrdiff_t __ivarOffset(Ivar v)
+{
+    return ivar_getOffset(v);
+}
+
+/* Working with Instance Variables */
+
+
+
+
+
+
+/* Working with Properties */
+
+const char *__propertyName(objc_property_t property)
+{
+    return property_getName(property);
+}
+
+const char *__propertyAttributes(objc_property_t property)
+{
+    return property_getAttributes(property);
+}
+objc_property_attribute_t *__propertyCopyAttributeList(objc_property_t property, unsigned int *outCount)
+{
+    return property_copyAttributeList(property, outCount);
+}
+
+char *__propertyCopyAttributeValue(objc_property_t property, const char *attributeName)
+{
+    return property_copyAttributeValue(property, attributeName);
+}
+
+/* Working with Properties */
+
+
+
+
+/* Working with Protocols */
+
+Protocol *__objcProtocol(const char *name)
+{
+    return objc_getProtocol(name);
+}
+
+Protocol * __unsafe_unretained *__objcCopyProtocolList(unsigned int *outCount)
+{
+    return objc_copyProtocolList(outCount);
+}
+BOOL __protocolConformsToProtocol(Protocol *proto, Protocol *other)
+{
+    return protocol_conformsToProtocol(proto, other);
+}
+BOOL __protocolEqual(Protocol *proto, Protocol *other)
+{
+    return protocol_isEqual(proto, other);
+}
+const char *__protocolName(Protocol *p)
+{
+    return protocol_getName(p);
+}
+struct objc_method_description __protocolMethodDescription(Protocol *p, SEL aSel, BOOL isRequiredMethod, BOOL isInstanceMethod)
+{
+    return protocol_getMethodDescription(p, aSel, isRequiredMethod, isInstanceMethod);
+}
+struct objc_method_description *__protocolCopyMethodDescriptionList(Protocol *p, BOOL isRequiredMethod, BOOL isInstanceMethod, unsigned int *outCount)
+{
+    return protocol_copyMethodDescriptionList(p, isRequiredMethod, isInstanceMethod, outCount);
+}
+
+objc_property_t __protocolProperty(Protocol *proto, const char *name, BOOL isRequiredProperty, BOOL isInstanceProperty)
+{
+    return protocol_getProperty(proto, name, isRequiredProperty, isInstanceProperty);
+}
+objc_property_t *__protocolCopyPropertyList(Protocol *proto, unsigned int *outCount)
+{
+    return protocol_copyPropertyList(proto, outCount);
+}
+Protocol *__unsafe_unretained *__protocolCopyProtocolList(Protocol *proto, unsigned int *outCount)
+{
+    return protocol_copyProtocolList(proto, outCount);
+}
+Protocol *__objcAllocateProtocol(const char *name)
+{
+    return objc_allocateProtocol(name);
+}
+
+void __objcRegisterProtocol(Protocol *proto)
+{
+    return objc_registerProtocol(proto);
+}
+
+void __protocolAddMethodDescription(Protocol *proto, SEL name, const char *types, BOOL isRequiredMethod, BOOL isInstanceMethod)
+{
+    return protocol_addMethodDescription(proto, name, types, isRequiredMethod, isInstanceMethod);
+}
+
+void __protocolAddProtocol(Protocol *proto, Protocol *addition)
+{
+    return protocol_addProtocol(proto, addition);
+}
+
+void __protocolAddProperty(Protocol *proto, const char *name, const objc_property_attribute_t *attributes, unsigned int attributeCount, BOOL isRequiredProperty, BOOL isInstanceProperty)
+{
+    return protocol_addProperty(proto, name, attributes, attributeCount, isRequiredProperty, isInstanceProperty);
+}
+
+/* Working with Protocols */
+
+
+
+/* Working with Libraries */
+
+const char **__objcCopyImageNames(unsigned int *outCount)
+{
+    return objc_copyImageNames(outCount);
+}
+const char *__clsImageName(Class cls)
+{
+    return class_getImageName(cls);
+}
+const char **__objcCopyClassNamesForImage(const char *image,
+                                         unsigned int *outCount)
+{
+    return objc_copyClassNamesForImage(image, outCount);
+}
+
+/* Working with Libraries */
+
+
+
+/* Working with Selectors */
+
+const char *__selName(SEL sel)
+{
+    return sel_getName(sel);
+}
+
+SEL __selUid(const char *str)
+{
+    return sel_getUid(str);
+}
+
+SEL __selRegisterName(const char *str)
+{
+    return sel_registerName(str);
+}
+
+BOOL __selEqual(SEL lhs, SEL rhs)
+{
+    return sel_isEqual(lhs, rhs);
+}
+
+/* Working with Selectors */
+
+
+/* Objective-C Language Features */
+
+void __objcEnumerationMutation(id obj)
+{
+    return objc_enumerationMutation(obj);
+}
+void objcSetEnumerationMutationHandler(void (*handler)(id))
+{
+    objc_setEnumerationMutationHandler(handler);
+}
+
+IMP __impImplementationWithBlock(id block)
+{
+    return imp_implementationWithBlock(block);
+}
+
+id __impBlock(IMP anImp)
+{
+    return imp_getBlock(anImp);
+}
+
+BOOL __impRemoveBlock(IMP anImp)
+{
+    return imp_removeBlock(anImp);
+}
+id __objcLoadWeak(id *location)
+{
+    return objc_loadWeak(location);
+}
+id __objcStoreWeak(id *location, id obj)
+{
+    return objc_storeWeak(location, obj);
+}
+
+/* Objective-C Language Features */
+
+
+/* Associative References */
+
+void __objcSetAssociatedObject(id object, const void *key, id value, objc_AssociationPolicy policy)
+{
+    return objc_setAssociatedObject(object, key, value, policy);
+}
+
+id __objcAssociatedObject(id object, const void *key)
+{
+    return objc_getAssociatedObject(object, key);
+}
+void __objcRemoveAssociatedObjects(id object)
+{
+    return objc_removeAssociatedObjects(object);
+}
+
+/* Associative References */
 
 #pragma mark
 - (NSDictionary*)mjInfo
